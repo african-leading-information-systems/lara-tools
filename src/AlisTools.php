c9d6213b3163f6156b90c3fd71d317c128c3cea6
Gabeta Soro <gabeta.soro@gmail.com>
2019-11-23 11:21:46 +0000

    arrayToString static method, translate array to string with unique value

--- src/AlisTools.php
+++ src/AlisTools.php
@@ -3,5 +3,17 @@ namespace Alis\LaraTools;
 
 class AlisTools
 {
-
+    /**
+     * @param string $source
+     * @param string $delimiter
+     * @return array
+     */
+    public static function arrayToString(string $source, string $delimiter): array
+    {
+        return array_filter(array_unique(array_map(function ($item) {
+            return trim($item);
+        }, explode($delimiter, $source))), function ($item) {
+            return !empty($item);
+        });
+    }
 }
