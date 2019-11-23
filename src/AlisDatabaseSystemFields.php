0f24102b7286d27a56746f8b1b09399d3b6f6d8d
Gabeta Soro <gabeta.soro@gmail.com>
2019-11-23 11:26:59 +0000

    generateSystemFields for generate Alis default system fields

--- src/AlisDatabaseSystemFields.php
+++ src/AlisDatabaseSystemFields.php
@@ -1,7 +1,20 @@
 <?php
 namespace Alis\LaraTools;
 
+use Illuminate\Database\Schema\Blueprint;
+
 class AlisDatabaseSystemFields
 {
-
+    /**
+     * @param Blueprint $table
+     * @param int $precision
+     */
+    public static function generateSystemFields(Blueprint $table, $precision = 0)
+    {
+        $table->bigInteger('creator')->index()->unsigned();
+        $table->timestamp('creation_date', $precision);
+        $table->bigInteger('lastkeyer')->index()->unsigned();
+        $table->timestamp('lastkey_date', $precision);
+        $table->boolean('invalid_y_n')->default(0);
+    }
 }
