6ce79c1672e7112f8f868fce6ee1febb20c547b6
Gabeta Soro <gabeta.soro@gmail.com>
2019-11-24 14:44:30 +0000

    Alis: Exist rule

---
+++ src/validator/AlisExistRule.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class AlisExistRule implements Rule
+{
+    private $table;
+    private $field;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($table, $field)
+    {
+        $this->table = $table;
+        $this->field = $field;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return !! DB::table($this->table)->where($this->field, $value)
+            ->where('invalid_y_n', 0)
+            ->first();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute not exist.';
+    }
+}
